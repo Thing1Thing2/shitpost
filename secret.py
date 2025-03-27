3712e651622839f292a950e25698ff443c9896c3
Thing1Thing2 <testrunmymails@gmail.com>
2025-03-27 17:32:59 +0800

    if I was your boyfriend, I wouldnt let you go

---
+++ secret.py
@@ -0,0 +1,8 @@
+print("Okay, okay... files. But don't expect them to be perfect. Like, who cares?")
+
+filename = f"unlimited_trauma.txt"
+with open(filename, "w") as f:
+    f.write("Hey bitch")
+    print(
+        f"Created file {filename}, and guess what? I still don't give a fuck about clean code."
+    )
