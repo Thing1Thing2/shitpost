a9839f783b7415927f0a2a47f813fd919fbdf5bd
Thing1Thing2 <testrunmymails@gmail.com>
2025-03-27 18:03:40 +0800

    hit me baby one more time

--- metal_health.py
+++ metal_health.py
@@ -1,3 +1,8 @@
 print(
     "I said goodbye before you came, I turned around and ran away. I felt too scared I should I have said.. helo helo helo helo."
 )
+
+# showing some serious signs of mental health issues
+# I am not okay
+# Maybe I am
+# who cares
