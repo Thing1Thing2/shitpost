12a6d48ddfd2922b00dd516834fd84011fda7ea2
Thing1Thing2 <testrunmymails@gmail.com>
2025-03-27 18:02:52 +0800

    do your fake smile

---
+++ vacation_bible_school.py
@@ -0,0 +1,9 @@
+ans = input("Do you agree with Ayesha Erotica supremacy? [y,n]")
+if ans == "y":
+    print("You are correct")
+else:
+    print("You are wrong")
+
+print(
+    f"\033[38;2;255;105;180mHalf the time i don't know what I am doing or saying. don't trust me broski.\033[0m"
+)
