62b708864c23a76ef430f68266638b910455d975
Thing1Thing2 <testrunmymails@gmail.com>
2025-03-27 17:01:45 +0800

    up yours clean code

---
+++ spring_breakers.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+print(
+    "Vroom vroom vroom vroom in a big pink truck. February 10th and Im gonna pull up. Hey "
+)
+
+# open url
+webbrowser.open("https://www.youtube.com/watch?v=nDYDzRWvDJA&ab_channel=Harmonexus")
