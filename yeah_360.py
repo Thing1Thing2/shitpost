5bd094487e79888502bdc078d9f1e539b84ccc9a
Thing1Thing2 <testrunmymails@gmail.com>
2025-03-27 16:37:42 +0800

    initiate trauma dumping
    even though you have like zero trauma inducing situations

---
+++ yeah_360.py
@@ -0,0 +1,7 @@
+print(
+    "Yeah 360, when you're in the mirror you're just looking at me. When you're in the mirror do you like what you see?"
+)
+
+print("366 froggy girls bumpin' that")
+
+print("Please leave me alone ffs")
